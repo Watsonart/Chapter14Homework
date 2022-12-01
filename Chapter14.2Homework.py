310b55188a7fe06d8626e6d4b2f7ca883702ca68
Watsonart <klimovartur@gmail.com>
2022-12-01 10:23:58 -0500

    14.2 ready

---
+++ Chapter14.2Homework.py
@@ -0,0 +1,26 @@
+#1. Slice the string in such a way that it will print only "Test".
+my_string = "Test Automation"
+
+#my_string[:4]
+
+word_test = my_string[:4]
+
+print(word_test)
+
+#2. Slice the string in such a way that it will print only "Automation".
+my_string = "Test Automation"
+
+#my_string[5:15]
+
+second_word = my_string[5:15]
+
+print(second_word)
+
+#3. Slice the second and third elements (50 and 20) in the list.
+my_list = [10,50,20,25,30]
+
+#Type your answer here
+
+sliced_list = my_list[1:3]
+
+print(sliced_list)
